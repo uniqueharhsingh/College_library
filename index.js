1db39767f3ef442067de17023d253d3b1c27e7b4
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2021-10-31 01:57:32 -0700

    update:layout table

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("In this We are creating a projects this is tutorial 33")
+
+//Creating Protypes using Constrctor
+function Book(name,author,type){
+    this.name=name;
+    this.author=author;
+    this.type=type;
+
+}
+
+//Adding method to display constructor
+
+
+//Adding EventListner to Add Books
+
+
+
+
+
+
+
+
+
+
+
+
+
+
