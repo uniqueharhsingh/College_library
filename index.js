eb271226a5586e69f64bd9eb60fce244a21b336d
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2021-10-31 21:35:37 -0700

    update:js

--- index.js
+++ index.js
@@ -11,7 +11,74 @@ function Book(name,author,type){
 //Adding method to display constructor
 
 
-//Adding EventListner to Add Books
+//Adding Method to display protoype
+
+
+//Adding submit event listner to the libraryform
+
+let libraryform=document.getElementById("libraryform")
+libraryform.addEventListener("submit",libraryformsubmit);
+
+function libraryformsubmit(e){
+    e.preventDefault()
+    console.log("You Have been CLiked");
+
+    let name=document.getElementById("bookname").value;
+    let author=document.getElementById("author").value;
+    
+    let type;
+
+    //Finction,cooking,Computer programming
+
+    let Fiction=document.getElementById("Fiction");
+    let computerProgramming=document.getElementById("ComputerProgramming");
+    let cooking=document.getElementById("cooking");
+
+    if (Fiction.checked){
+        type=Fiction.value;
+    }
+    else if (cooking.checked){
+        type=cooking.value
+
+    }
+    else if(computerProgramming.checked){
+        type=computerProgramming.value
+    }
+
+
+    let Books=new Book(name,author,type)
+    console.log(Books);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+ 
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
